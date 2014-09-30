d13aa41fee0aa6936101151f8d36bdf87fadce56
Vivek <vivek@vivek-Inspiron-3537.(none)>
2014-10-01 01:04:48 +0530

    Employee app

---
+++ employee/js/Collection/emp_collections.js
@@ -0,0 +1,21 @@
+var app = app || {};
+
+var empList = Backbone.Collection.extend({
+	
+	model: app.Emp,
+	localStorage: new Backbone.LocalStorage('employees-bacbone'),
+	
+	nextOrder: function(){
+		if ( !this.length) {
+			return 1;
+		}
+		return this.last().get('order') + 1;
+	},
+		
+	comparator: function(emp) {
+		return emp.get('order');	
+	}
+	
+});
+
+app.Employees = new empList();
---
+++ employee/js/Model/emp_models.js
@@ -0,0 +1,11 @@
+var app = app || {};
+
+app.Emp = Backbone.Model.extend({
+	defaults:{
+		id: '',
+		name: '',
+		age: '',
+		salary: ''
+	}
+	
+});
