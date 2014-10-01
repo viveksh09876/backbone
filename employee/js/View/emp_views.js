deed59ed7ef1235db8b1353ca52d43914fcd87c1
Vivek Sharma <vivek@XT-PHP-129.(none)>
2014-10-01 17:50:08 +0530

    files updated

--- employee/js/View/emp_views.js
+++ employee/js/View/emp_views.js
@@ -3,16 +3,30 @@ var app = app || {};
 (function(){
 app.AppView = Backbone.View.extend({
 	el: '#emp_app',
+	template: _.template($('#list_template').html()),
 	initialize: function() {		
 		
 		this.$empid = this.$('#emp_id');
 		this.$empname = this.$('#emp_name');
 		this.$empage = this.$('#emp_age');
 		this.$empsalary = this.$('#emp_salary');
-		this.$main = this.$('#list_employees');
+		this.$list = this.$('#info_list');
+		this.listenTo(app.Employees, 'add', this.addOne);
 	},
+		
 	events: {
 		'click #save_employee' : 'addEmployee'
+	},
+	render: function(){
+		//console.log(this.model.toJSON());
+		 this.$el.html(this.template(this.model.toJSON()));
+		 return this;
+	},
+	addOne: function (Emp) {
+		
+		var view = new app.AppView({ model: Emp });
+		console.log(view.render().el);
+		this.$list.append(view.render().el);
 	},
 	
 	newEntries: function() {
@@ -21,14 +35,15 @@ app.AppView = Backbone.View.extend({
 			name : this.$empname.val().trim(),
 			age : this.$empage.val().trim(),
 			salary : this.$empsalary.val().trim()
-		}
+		};
 	},
 	
-	addEmployee: function(){		
-		if(this.$empid.val().trim() && this.$empname.val().trim() && this.$empage.val().trim() && this.$empsalary.val().trim()) {			
+	addEmployee: function(){	
+			
+		if(this.$empid.val().trim() || this.$empname.val().trim() || this.$empage.val().trim() || this.$empsalary.val().trim()) {			
 			app.Employees.create(this.newEntries());
 		}else{
-			alert(1);
+			alert('Please fill all information');
 		}
 	}	
 	
